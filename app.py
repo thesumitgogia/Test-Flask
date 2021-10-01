5d8c308576f9d51ffe117b5a2970389ce1d13f31
Sumit Gogia <85780119+thesumitgogia@users.noreply.github.com>
2021-10-01 19:32:55 +0530

    Changes

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def hello_world():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='https://thesumitgogia.github.io/Test-Flask/')
